3ef4bce06cb00927001c2dcf11f09636e99822a1
gonjavi <javiercaliescali@gmail.com>
2020-05-16 18:20:42 -0500

    sign in set up

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/navbar';
 import Dashboard from './components/dashboard/dashboard';
 import ProjectDetail from './components/projects/ProjectDetail';
+import SignIn from './components/auth/SignIn';
 
 class App extends Component {
   render() {
@@ -12,8 +13,8 @@ class App extends Component {
           <Navbar />
           <Switch>
             <Route  exact path='/' component={Dashboard} />
-            {/* react route attach component  */} 
             <Route path='/project/:id' component={ProjectDetail} />
+            <Route path='/signin' component={SignIn} />
           </Switch>
         </div>
       </BrowserRouter>
