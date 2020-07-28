f32307e499c907b68e16f690605396a08b3f68a9
manci <javiercaliescali@gmail.com>
2020-07-28 13:12:06 -0500

    install redux thunk

--- src/components/projects/projectSummary.js
+++ src/components/projects/projectSummary.js
@@ -4,7 +4,7 @@ const ProjectSummary = ({project}) => {
   return (
     <div className="card z-depth-0 project- summary">
       <div className="card-content grey-text text-darken-3">
-  <span className="card-title">{project.title}</span>
+        <span className="card-title">{project.title}</span>
         <p> Posted by G.J.</p>
         <p className="gey-text">15th May, 5pm</p>
       </div>
